3220e01394f42733eccf0f727fd1c787ba625299
EUViegas2 <viegas_rv24@hotmail.com>
2024-05-17 17:20:51 +0100

    Vec2 updates

--- Vec2.cpp
+++ Vec2.cpp
@@ -12,25 +12,34 @@ Vec2::Vec2(float xin, float yin)
 
 }
 
+bool Vec2::operator==(const Vec2& rhs) const
+{
+	return this->x == rhs.x && this->y == rhs.y;
+}
+
+bool Vec2::operator!=(const Vec2& rhs) const
+{
+	return this->x != rhs.x || this->y != rhs.y;
+}
+
 Vec2 Vec2::operator + (const Vec2& rhs) const
 {
-
-	return Vec2();
+	return Vec2(this->x+rhs.x, this->y+rhs.y)
 }
 
 Vec2 Vec2::operator-(const Vec2& rhs) const
 {
-	return Vec2();
+	return Vec2(this->x - rhs.x, this->y - rhs.y);
 }
 
 Vec2 Vec2::operator/(const float val) const
 {
-	return Vec2();
+	return Vec2(this->x / val, this->y / val);
 }
 
 Vec2 Vec2::operator*(const float val) const
 {
-	return Vec2();
+	return Vec2(this->x * val, this->y * val);
 }
 
 Vec2 Vec2::operator+=(const Vec2& rhs) const
